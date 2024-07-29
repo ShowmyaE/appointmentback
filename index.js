e24978df34cce02a61e15a0f69f78ee69077185e
showmyaE <showmya3@gmail.com>
2024-07-29 09:44:14 +0530

    add

--- index.js
+++ index.js
@@ -13,107 +13,111 @@ app.use(express.json())
 const dbPath=path.join(__dirname,'demodb.db')
 let db=null
 
-const initializeDbAndServer=async()=>{
-    try{
-        db=await open({
-            filename:dbPath,
-            driver:sqlite3.Database
-        })
+// const initializeDbAndServer=async()=>{
+//     try{
+//         db=await open({
+//             filename:dbPath,
+//             driver:sqlite3.Database
+//         })
 
-    }catch(error){
-        console.log(`DB Error : ${error.message}`)
-        process.exit(1)
-  }
-}
+//     }catch(error){
+//         console.log(`DB Error : ${error.message}`)
+//         process.exit(1)
+//   }
+// }
 app.listen(4000,()=>{
   console.log('Server Running at http://localhost:4000')
    
 })
-initializeDbAndServer()
+
 app.get('/',async(req,res)=>{
-    const getUserQuery=`select * from registration`;
-    const userDbDetails=await db.all(getUserQuery);
-    console.log('DB value',userDbDetails);
-    res.send(userDbDetails)
-
+  res.send("HEllo")
 })
-const authentication = (request, response, next) => {
-    let jwtToken
-    console.log("AUTH", request.headers['authorization'])
-    const authHeader = request.headers['authorization']
-    if (authHeader) {
-      jwtToken = authHeader.split(' ')[1]
-    }
-    console.log("JWT", jwtToken)
+// initializeDbAndServer()
+// app.get('/',async(req,res)=>{
+//     const getUserQuery=`select * from registration`;
+//     const userDbDetails=await db.all(getUserQuery);
+//     console.log('DB value',userDbDetails);
+//     res.send(userDbDetails)
 
-    if (jwtToken) {
-      jwt.verify(jwtToken, 'SECRET_KEY', (error, payload) => {
-        if (error) {
-          response.status(401)
-          response.send('Invalid JWT Token')
-        } else {
-          request.email = payload.email
-          request.userId = payload.userId
-          next()
-        }
-      })
-    } else {
-      response.status(401)
-      response.send('Invalid JWT Token')
-    }
-  }
+// })
+// const authentication = (request, response, next) => {
+//     let jwtToken
+//     console.log("AUTH", request.headers['authorization'])
+//     const authHeader = request.headers['authorization']
+//     if (authHeader) {
+//       jwtToken = authHeader.split(' ')[1]
+//     }
+//     console.log("JWT", jwtToken)
+
+//     if (jwtToken) {
+//       jwt.verify(jwtToken, 'SECRET_KEY', (error, payload) => {
+//         if (error) {
+//           response.status(401)
+//           response.send('Invalid JWT Token')
+//         } else {
+//           request.email = payload.email
+//           request.userId = payload.userId
+//           next()
+//         }
+//       })
+//     } else {
+//       response.status(401)
+//       response.send('Invalid JWT Token')
+//     }
+//   }
   
 
-app.post("/signUp",async(req,res)=>{
-    const {email, password} = req.body
-    console.log('DB value',email,password)
-    const id = uuidv4();
-     const insertquery=`INSERT INTO registration(CustomerId, EmailId, Password)
-    VALUES ('${id}', '${email}','${password}')`;
-    const insertData=await db.run(insertquery)
-if(insertData) {
-    res.send({data:"successfully inserted"})
-}
-else{
-    res.status(400)
-    res.send({data:"Failed to inserted"})
-}
-})
+// app.post("/signUp",async(req,res)=>{
+//     const {email, password} = req.body
+//     console.log('DB value',email,password)
+//     const id = uuidv4();
+//      const insertquery=`INSERT INTO registration(CustomerId, EmailId, Password)
+//     VALUES ('${id}', '${email}','${password}')`;
+//     const insertData=await db.run(insertquery)
+// if(insertData) {
+//     res.send({data:"successfully inserted"})
+// }
+// else{
+//     res.status(400)
+//     res.send({data:"Failed to inserted"})
+// }
+// })
 
-app.post("/loginIn",async(req,res)=>{
-    const {email, password} = req.body
-    console.log('DB value',email,password)
-    const getUserQuery = `SELECT * FROM registration WHERE EmailId='${email}' AND Password='${password}';`
-    const userDbDetails = await db.get(getUserQuery)
-    console.log(userDbDetails)
-    if (userDbDetails) {
-        const payload = {email:email,userId:userDbDetails.CustomerId}
-        const jwtToken = jwt.sign(payload, 'SECRET_KEY')
-        res.send({jwtToken})
-      } else {
-        res.status(400)
-        res.send('Invalid password')
-      }
-})
+// app.post("/loginIn",async(req,res)=>{
+//     const {email, password} = req.body
+//     console.log('DB value',email,password)
+//     const getUserQuery = `SELECT * FROM registration WHERE EmailId='${email}' AND Password='${password}';`
+//     const userDbDetails = await db.get(getUserQuery)
+//     console.log(userDbDetails)
+//     if (userDbDetails) {
+//         const payload = {email:email,userId:userDbDetails.CustomerId}
+//         const jwtToken = jwt.sign(payload, 'SECRET_KEY')
+//         res.send({jwtToken})
+//       } else {
+//         res.status(400)
+//         res.send('Invalid password')
+//       }
+// })
 
 
 
-app.post('/bookingDetail', authentication, async(req,res) => {
-    const id = uuidv4();
-    const customerId=req.userId
-    console.log("req.body", req.body)
-    const {name,email, company,phonenumber,domain,interests, bookingDate} = req.body
-    const insertquery=`INSERT INTO bookingDetail (Id, CustomerId,Name,EmailId,Company,PhoneNumber,Domain,Interests,BookingDate,Date)
-    VALUES ('${id}', '${customerId}','${name}','${email}','${company}','${phonenumber}','${domain}','${interests}','${bookingDate}','${bookingDate}' )`;
-    const userDbDetails=await db.run(insertquery)
-    console.log("INSERTQUERY", userDbDetails)
-    res.send("SUCCESS")
-})
+// app.post('/bookingDetail', authentication, async(req,res) => {
+//     const id = uuidv4();
+//     const customerId=req.userId
+//     console.log("req.body", req.body)
+//     const {name,email, company,phonenumber,domain,interests, bookingDate} = req.body
+//     const insertquery=`INSERT INTO bookingDetail (Id, CustomerId,Name,EmailId,Company,PhoneNumber,Domain,Interests,BookingDate,Date)
+//     VALUES ('${id}', '${customerId}','${name}','${email}','${company}','${phonenumber}','${domain}','${interests}','${bookingDate}','${bookingDate}' )`;
+//     const userDbDetails=await db.run(insertquery)
+//     console.log("INSERTQUERY", userDbDetails)
+//     res.send("SUCCESS")
+// })
 
-app.get('/getbookingDetail', authentication, async (req,res) => {
-    const customerId=req.userId
-     const getUserQuery = `SELECT * FROM bookingDetail WHERE CustomerId='${customerId}' ;`
-     const insertData=await db.all(getUserQuery)
-     console.log("GET CUSTOMER", insertData)
-    res.send(insertData)
-})
+// app.get('/getbookingDetail', authentication, async (req,res) => {
+//     const customerId=req.userId
+//      const getUserQuery = `SELECT * FROM bookingDetail WHERE CustomerId='${customerId}' ;`
+//      const insertData=await db.all(getUserQuery)
+//      console.log("GET CUSTOMER", insertData)
+//     res.send(insertData)
+// })
